b226c8ee0aa24d365338ddd0c9370d81e885c72d
Sofiia <s.solnichkova@gmail.com>
2022-05-25 19:03:15 +0300

    final

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function show() {
+    const registration = document.querySelector("#registration");
+    if (registration.style.display === 'none') {
+        registration.style.display = 'block';
+    } else {
+        registration.style.display = 'none';
+    }
+}
+
+// const btn = document.querySelector("#show-form");
+// btn.addEventListener('click', function handleClick () {
+    
+// });
